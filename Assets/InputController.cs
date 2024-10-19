97d4e67a78524d58d77a42d75d40364a9f94459a
Tonyy456 <ajdalesandro0115@gmail.com>
2024-10-19 11:37:16 -0400

    adding basic input system and movement

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputController : MonoBehaviour
 {
+    [SerializeField] private PlayerController controller;
+
+    private InputSystem inputSystem;
+    private InputAction move;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        inputSystem = new InputSystem();
+        move = inputSystem.FindAction("Move");
+        move.performed += OnMovePerformed;
+        move.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMovePerformed(InputAction.CallbackContext e)
     {
-        
+        Vector2 keyPressDir = move.ReadValue<Vector2>();
+        controller?.Move(keyPressDir);
     }
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed = 1;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.gameObject.transform.position += new Vector3(0, 10, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector2 dir)
     {
-        
+        Vector2 dp_2d = dir * Time.deltaTime * speed;
+        Vector3 dp = new Vector3(dp_2d.x, dp_2d.y);
+        this.transform.position += dp;
     }
 }
